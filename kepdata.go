386ab6a413dc8cda8a05c69de69881d255fe6640
Diego Armando <diarsaba@Mac-mini-de-Diego.local>
2022-01-05 14:33:53 -0600

    full collection handler

--- kepdata.go
+++ kepdata.go
@@ -2,32 +2,14 @@ package kepdata
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"strings"
 
-	//"strings"
-	"crypto/sha256"
-	"encoding/json"
-
 	"github.com/syndtr/goleveldb/leveldb"
 )
-
-// Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
-	if name == "" {
-		return "", errors.New("empty name")
-	}
-
-	// If a name was received, return a value that embeds the name
-	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
-}
-
-//keys := strings.Split(key, ",")
 
 type KPD struct {
 	name     string
@@ -35,20 +17,177 @@ type KPD struct {
 	spliter  string
 }
 
+//return "", errors.New("empty name")
 func DB(ms string, spliter string, fragment uint8) KPD {
 	return KPD{ms, fragment, spliter}
 }
 
-func (kpd *KPD) fridke(pre []byte, key []byte) []byte {
-	hash := sha256.Sum256(append(pre, key...))
-	return hash[:kpd.fragment]
+func (kpd *KPD) MapCollection(key string, private string, data string) ([]byte, error) {
+	var akey map[string]string
+	//var akey map[byte]byte
+	if err := json.Unmarshal([]byte(key), &akey); err != nil {
+		return nil, errors.New("invalid key JSON string")
+	}
+	var aprivate map[string]string
+	if err := json.Unmarshal([]byte(private), &aprivate); err != nil {
+		return nil, errors.New("invalid private JSON string")
+	}
+	var adata map[string]string
+	if err := json.Unmarshal([]byte(data), &adata); err != nil {
+		return nil, errors.New("invalid data JSON string")
+	}
+	return kpd.collection(akey, aprivate, adata), nil
 }
+func (kpd *KPD) CollectionByPrimaryKey(pre []byte, key []byte) ([]byte, error) {
 
-func (kpd *KPD) primarykey(pre []byte, key []byte) []byte {
+	db, err := leveldb.OpenFile(kpd.name, nil)
+	if err != nil {
+		return nil, errors.New("Failed to load or create database")
+	}
+	defer db.Close()
+
+	primarykey := kpd.fridke(pre, key)
+
+	collectionID, err := db.Get(primarykey, nil)
+	if err != nil {
+		return nil, errors.New("No existe esta llave")
+	}
+	data, err := db.Get(collectionID, nil)
+	if err != nil {
+		return nil, errors.New("No existe la colleccion")
+	}
+	return data, nil
+}
+func (kpd *KPD) CollectionKey(name string) [][]byte {
+
+	db, err := leveldb.OpenFile(kpd.name, nil)
+	if err != nil {
+	}
+	defer db.Close()
+
+	tempkey := kpd.fridke([]byte("key:"), []byte(name))
+	collectionword, err := db.Get(tempkey, nil)
+	if err != nil {
+	}
+
+	step := int(kpd.fragment)
+
+	var collection [][]byte
+
+	for i := 0; i < len(collectionword); i += step {
+		temp, err := db.Get(collectionword[i:i+step], nil)
+		if err != nil {
+		} else {
+			collection = append(collection, temp)
+		}
+	}
+	return collection
+}
+func (kpd *KPD) CollectionWord(name string) [][]byte {
+
+	db, err := leveldb.OpenFile(kpd.name, nil)
+	if err != nil {
+	}
+	defer db.Close()
+
+	tempkey := kpd.fridke([]byte("word:"), []byte(name))
+	collectionword, err := db.Get(tempkey, nil)
+	if err != nil {
+	}
+
+	step := int(kpd.fragment)
+
+	var collection [][]byte
+
+	for i := 0; i < len(collectionword); i += step {
+		temp, err := db.Get(collectionword[i:i+step], nil)
+		if err != nil {
+		} else {
+			collection = append(collection, temp)
+		}
+	}
+	return collection
+}
+func (kpd *KPD) CollectionContains(key string, word string) [][]byte {
+	db, err := leveldb.OpenFile(kpd.name, nil)
+	if err != nil {
+	}
+	defer db.Close()
+
+	words := strings.Split(word, " ")
+
+	keyb := kpd.fridke([]byte("key:"), []byte(key))
+	collectionkey, err := db.Get(keyb, nil)
+	if err != nil {
+		return make([][]byte, 0)
+	}
+
+	ready := make(map[string]string)
+	var collection [][]byte
+	step := int(kpd.fragment)
+	for i := 0; i < len(collectionkey); i += step {
+		for _, w := range words {
+			wordb := kpd.fridke([]byte("word:"), []byte(w))
+			Wordcollection, err := db.Get(wordb, nil)
+			if err != nil {
+				return make([][]byte, 0)
+			}
+			tempk := collectionkey[i : i+step]
+			temps := string(tempk)
+			if _, ok := ready[temps]; !ok {
+				if bytes.Contains(Wordcollection, tempk) {
+					ready[temps] = ""
+					temp, err := db.Get(tempk, nil)
+					if err != nil {
+					} else {
+						collection = append(collection, temp)
+					}
+				}
+			}
+		}
+	}
+	return collection
+}
+func (kpd *KPD) UpdateKeyCollection(key string) error {
+	//TODO: change the principal PrimariKey of collection to new
+	//this version only add new PrimaryKeys
+	db, err := leveldb.OpenFile(kpd.name, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	var akey map[string]string
+	//var akey map[byte]byte
+	if err := json.Unmarshal([]byte(key), &akey); err != nil {
+		panic(err)
+	}
+
+	var k, v string
+	for k, v = range akey {
+		break
+	}
+	keyWordID := kpd.fridke([]byte(k), []byte(v))
+	collectionID, err := db.Get(keyWordID, nil)
+
+	//TODO:
+	for k, v := range akey {
+		tempkeyWordID := kpd.fridke([]byte(k), []byte(v))
+		tempcollectionID, err := db.Get(keyWordID, nil)
+		if err != nil {
+			db.Put(tempkeyWordID, collectionID, nil)
+		} else if bytes.Compare(collectionID, tempcollectionID) == 0 {
+			log.Println("posible error primary key exist")
+		}
+	}
+	return nil
+}
+func (kpd *KPD) RemoveCollection(pre []byte, key []byte) error {
 
 	db, err := leveldb.OpenFile(kpd.name, nil)
 	if err != nil {
 		log.Println("err failed to load or create database")
+		return nil
 	}
 	defer db.Close()
 
@@ -57,32 +196,44 @@ func (kpd *KPD) primarykey(pre []byte, key []byte) []byte {
 	collectionID, err := db.Get(primarykey, nil)
 	if err != nil {
 		log.Println("no existe esta llave")
+		return nil
 	}
 	data, err := db.Get(collectionID, nil)
 	if err != nil {
 		log.Println("no existe la colleccion")
+		return nil
 	}
-	return data
-}
 
-func (kpd *KPD) MapCollection(key string, private string, data string) []byte {
-	var akey map[string]string
-	//var akey map[byte]byte
-	if err := json.Unmarshal([]byte(key), &akey); err != nil {
+	var tasher map[string]string
+	if err := json.Unmarshal(data, &tasher); err != nil {
 		panic(err)
 	}
-	var aprivate map[string]string
-	if err := json.Unmarshal([]byte(private), &aprivate); err != nil {
-		panic(err)
-	}
-	var adata map[string]string
-	if err := json.Unmarshal([]byte(data), &adata); err != nil {
-		panic(err)
-	}
-	return kpd.Createcollection(akey, aprivate, adata)
-}
 
-func (kpd *KPD) Createcollection(key map[string]string, private map[string]string, data map[string]string) []byte {
+	for k, v := range tasher {
+
+		tempv := kpd.fridke([]byte("key:"), []byte(k))
+		collectionval, err := db.Get(tempv, nil)
+		if err != nil {
+		} else {
+			new := bytes.Replace(collectionval, collectionID, make([]byte, 0), 1)
+			db.Put(tempv, new, nil)
+		}
+
+		splits := strings.Split(v, kpd.spliter)
+		for _, s := range splits {
+			tempv := kpd.fridke([]byte("word:"), []byte(s))
+			collectionval, err := db.Get(tempv, nil)
+			if err != nil {
+			} else {
+				new := bytes.Replace(collectionval, collectionID, make([]byte, 0), 1)
+				db.Put(tempv, new, nil)
+			}
+		}
+	}
+
+	return db.Delete(collectionID, nil)
+}
+func (kpd *KPD) collection(key map[string]string, private map[string]string, data map[string]string) []byte {
 	db, err := leveldb.OpenFile(kpd.name, nil)
 	if err != nil {
 	}
@@ -167,110 +318,16 @@ func (kpd *KPD) Createcollection(key map[string]string, private map[string]strin
 	d, _ := json.Marshal(collection)
 	db.Put(collectionID, d, nil)
 
-	kpd.remw(rem_v, collectionID, db) //TODO rem no work
-	kpd.addw(new_v, collectionID, db)
+	kpd.remword(rem_v, collectionID, db) //TODO rem no work
+	kpd.addword(new_v, collectionID, db)
 	kpd.indexer(newpair, collectionID, db)
 	return collectionID
 }
-func (kpd *KPD) Keycollection(name string) [][]byte {
-
-	db, err := leveldb.OpenFile(kpd.name, nil)
-	if err != nil {
-	}
-	defer db.Close()
-
-	tempkey := kpd.fridke([]byte("key:"), []byte(name))
-	collectionword, err := db.Get(tempkey, nil)
-	if err != nil {
-	}
-
-	step := int(kpd.fragment)
-
-	var collection [][]byte
-
-	for i := 0; i < len(collectionword); i += step {
-		temp, err := db.Get(collectionword[i:i+step], nil)
-		if err != nil {
-		} else {
-			collection = append(collection, temp)
-		}
-	}
-	return collection
+func (kpd *KPD) fridke(pre []byte, key []byte) []byte {
+	hash := sha256.Sum256(append(pre, key...))
+	return hash[:kpd.fragment]
 }
-func (kpd *KPD) Wordcollection(name string) [][]byte {
-
-	db, err := leveldb.OpenFile(kpd.name, nil)
-	if err != nil {
-	}
-	defer db.Close()
-
-	tempkey := kpd.fridke([]byte("word:"), []byte(name))
-	collectionword, err := db.Get(tempkey, nil)
-	if err != nil {
-	}
-
-	step := int(kpd.fragment)
-
-	var collection [][]byte
-
-	for i := 0; i < len(collectionword); i += step {
-		temp, err := db.Get(collectionword[i:i+step], nil)
-		if err != nil {
-		} else {
-			collection = append(collection, temp)
-		}
-	}
-	return collection
-}
-func (kpd *KPD) Contains(key string, word string) [][]byte {
-	db, err := leveldb.OpenFile(kpd.name, nil)
-	if err != nil {
-	}
-	defer db.Close()
-
-	words := strings.Split(word, " ")
-
-	keyb := kpd.fridke([]byte("key:"), []byte(key))
-	collectionkey, err := db.Get(keyb, nil)
-	if err != nil {
-		return make([][]byte, 0)
-	}
-
-	ready := make(map[string]string)
-	var collection [][]byte
-	step := int(kpd.fragment)
-	for i := 0; i < len(collectionkey); i += step {
-		for _, w := range words {
-			wordb := kpd.fridke([]byte("word:"), []byte(w))
-			Wordcollection, err := db.Get(wordb, nil)
-			if err != nil {
-				return make([][]byte, 0)
-			}
-			tempk := collectionkey[i : i+step]
-			temps := string(tempk)
-			if _, ok := ready[temps]; !ok {
-				if bytes.Contains(Wordcollection, tempk) {
-					ready[temps] = ""
-					temp, err := db.Get(tempk, nil)
-					if err != nil {
-					} else {
-						collection = append(collection, temp)
-					}
-				}
-			}
-		}
-	}
-	return collection
-}
-
-func changePrimarys() {
-
-}
-func removeKey() {
-
-}
-
-func (kpd *KPD) addw(data []string, collectionID []byte, db *leveldb.DB) {
+func (kpd *KPD) addword(data []string, collectionID []byte, db *leveldb.DB) {
 
 	for _, str := range data {
 		splits := strings.Split(str, kpd.spliter)
@@ -285,8 +342,7 @@ func (kpd *KPD) addw(data []string, collectionID []byte, db *leveldb.DB) {
 		}
 	}
 }
-
-func (kpd *KPD) remw(data []string, collectionID []byte, db *leveldb.DB) {
+func (kpd *KPD) remword(data []string, collectionID []byte, db *leveldb.DB) {
 	for _, str := range data {
 
 		splits := strings.Split(str, kpd.spliter)
@@ -301,7 +357,6 @@ func (kpd *KPD) remw(data []string, collectionID []byte, db *leveldb.DB) {
 		}
 	}
 }
-
 func (kpd *KPD) indexer(data map[string]string, collectionID []byte, db *leveldb.DB) {
 
 	for k, v := range data {
